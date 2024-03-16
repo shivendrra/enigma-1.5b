c2decd88b67be0d3adc8c569aba37a268db55783
Shivendra <shivharsh44@gmail.com>
2024-03-16 21:11:08 +0530

    fixed the tokenizer and completed the training

--- dna_test.py
+++ dna_test.py
@@ -6,29 +6,32 @@ os.chdir(current_dir)
 from tokenizer import DNAtokenizer
 
 start = timeit.default_timer()
-with open('../parquet files/extra.txt', 'r', encoding='utf-8') as f:
+with open('../parquet files/train.txt', 'r', encoding='utf-8') as f:
   data = f.read()
 
 start_token = timeit.default_timer()
 print(f"file opened in {((start_token - start)/60):.2f} mins")
 
-model_prefix = '../tokenizer/trained models/base_5k'
+model_prefix = '../tokenizer/trained models/base_2k'
 os.makedirs(os.path.dirname(model_prefix), exist_ok=True)
 
 token = DNAtokenizer()
-token.train(data, 200)
+# token.train(data, 250)
 
-# token.load_model(model_path='../tokenizer/trained models/base_5k.model')
-# token.continue_train(data, 500)
+token.load_model(model_path='../tokenizer/trained models/base_1k.model')
+# token.continue_train(data, 7)
+# token.save_model(model_prefix=model_prefix)
 
-token.save_model(model_prefix=model_prefix)
 end_token = timeit.default_timer()
 print(f"tokenized in {((end_token - start_token)/60):.2f} mins")
 
-sample = 'CCTCCTGCCTGGAACATCAGGCTCCATGTTCTTTGGCTTTTAGAC'
-# with open('train files/new_dna_1.txt', 'r', encoding='utf-8') as f:
-#   sample = f.read()
+# sample = 'CCTCCTGCCTGGAACATCAGGCTCCATGTTCTTTGGCTTTTAGAC'
+with open('../enigma/new_dna.txt', 'r', encoding='utf-8') as f:
+  sample = f.read()
 
-print(token.encode(sample))
-print(sample == token.decode(token.encode(sample)))
-print(f"compression ration: {(len(sample) / len(token.encode(sample))):.2f}x")
+# print(token.encode(sample))
+encoded_tokens = token.encode(sample)
+decoded_tokens = token.decode(encoded_tokens)
+print(sample == decoded_tokens)
+print(f"file length: {len(sample)} \ntokens: {len(encoded_tokens)}")
+print(f"compression ration: {(len(sample) / len(encoded_tokens)):.2f}x")
