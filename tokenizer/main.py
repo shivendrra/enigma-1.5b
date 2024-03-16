e72bed4a1c4c26f7adca3a5fc0a6fafc0cc1529b
Shivendra <shivharsh44@gmail.com>
2024-03-16 22:14:37 +0530

    added some comments

--- tokenizer/main.py
+++ tokenizer/main.py
@@ -1,3 +1,12 @@
+"""
+  - basic bpe-tokenizer that doesn't uses byte pairing, insted uses set of initial unique characters
+    to train the new vocab
+  - set of initial characters = ["\n", "A", "C", "G", "T", " "] that can be present in a file or are
+    needed for the tokenizer
+  - save and load functions, saves two files, '.model' and 'vocab.json' and only '.model' file is loaded
+    'vocab.json' is just for human interpretation
+"""
+
 from tqdm import tqdm
 import json
 import os
@@ -6,6 +15,13 @@ os.chdir(current_dir)
 
 class DNAtokenizer:
   def __init__(self):
+    """
+      inital variables:
+        - chars = set of unique characters that could be present in the file, that are needed
+        - merges, vocab = empty dictonaries to store future merges and final vocab
+        - vocab_size = initially it's equal to 6 or len(chars), updated later
+        - str_to_idx, idx_to_str = functions enumerate chars to idx and idx to chars
+    """
     super().__init__()
     self.chars = ["\n", "A", "C", "G", "T", " "]
     self.vocab_size = len(self.chars)
@@ -15,10 +31,18 @@ class DNAtokenizer:
     self.index_to_string = {idx: char for idx, char in enumerate(self.chars)}
   
   def _encode(self, string):
+    """
+      encoder: takes a string, returns a list of integers
+        eg. AATGC --> ['2', '2', '5', '4', '3']
+    """
     encoded = [self.string_to_index[char] for char in string]
     return encoded
   
   def _decode(self, integer):
+    """
+      decoder: takes a list of integers, returns a string
+        eg. ['2', '2', '5', '4', '3'] --> AATGC
+    """
     decoded = ''.join([self.index_to_string[i] for i in integer])
     return decoded
 
@@ -50,9 +74,24 @@ class DNAtokenizer:
     return new_ids
 
   def _build_vocab(self):
+    """
+      it was causing some bugs, if not used, so I had to use it
+    """
     return {i: ids for i, ids in enumerate(self.chars)}
 
   def train(self, train_data, target_vocab):
+    """
+      - takes in the data, encodes it using _encode() function, converts each unique char to index
+          eg. AATGC --> ['2', '2', '5', '4', '3']
+      - performs iteration till n_merges i.e. target_vocab - self.vocab_size
+      - each iteration, makes dictonary of 2 consecutive pairs and then merges the max occuring
+        pair together
+      - at the end uses merges to build final vocab
+
+      Args:
+        train_data (str): a big file containing lots of dna sequence
+        target_vocab (integer): name tells you fucking idiot
+    """
     vocab = self._build_vocab()
     tokens = self._encode(train_data)
     ids = list(tokens)
@@ -74,6 +113,19 @@ class DNAtokenizer:
     self.vocab_size = len(vocab)
   
   def continue_train(self, train_data, n_merges):
+    """
+      - takes in the data, performs iteration till n_merges
+      - continues from the last index of the loaded merges
+      - each iteration, makes dictonary of 2 consecutive pairs and then merges the max occuring
+        pair together (same as train())
+      - at the end uses merges to build final vocab
+
+      Args:
+        train_data (str): a big file containing lots of dna sequence
+        n_merges (integer): no of merges
+      
+      ** this function has some problems
+    """
     tokens = self._encode(train_data)
     ids = list(tokens)
     for i in tqdm(range(n_merges), desc='Training continue'):
@@ -89,6 +141,14 @@ class DNAtokenizer:
     self.vocab_size = len(self.vocab)
   
   def encode(self, text):
+    """
+      - takes in the input string, encodes it using initial vocab '_encode()' function
+      - fetches merges from saved or loaded merges
+      
+      Args:
+        train_data (str): string of dna sequence
+        self.merges (dictonary): contains merges
+    """
     tokens = self._encode(text)
     ids = list(tokens)
     while len(ids) >= 2:
@@ -107,6 +167,16 @@ class DNAtokenizer:
     return text
   
   def save_model(self, model_prefix):
+    """
+      - basic save_model() funtion, saves two files, '.model' & 'vocab.json'
+      - '.model' contians all the final merges, each on next line
+      - 'vocab.json' contians the final vocab, for human interpretation
+
+      Args:
+        model_prefix (str): prefix along with the path
+        self.merges (dict): contains final merges
+        self.vocab (dict): contains final vocab
+    """
     model_file = model_prefix + '.model'
 
     with open(model_file, 'w', encoding='utf-8') as fwrite:
@@ -118,6 +188,14 @@ class DNAtokenizer:
     print('model file saved successfully!')
 
   def load_model(self, model_path):
+    """
+      - loads the '.model' file
+      - re-writes the merges in the new merges dict
+      - builds the vocab again for further use
+
+      Args:
+        model_path (str): path to the '.model' file
+    """
     assert model_path.endswith('.model')
 
     merges = {}
