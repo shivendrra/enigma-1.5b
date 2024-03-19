567ea2dd457945cd4102f6d71c91b270cc90e6b1
Shivendra <shivharsh44@gmail.com>
2024-03-19 23:30:24 +0530

    modified the tokenization process

--- enigma/generate.py
+++ enigma/generate.py
@@ -2,20 +2,15 @@ import os
 current_directory = os.path.dirname(os.path.abspath(__file__))
 os.chdir(current_directory)
 
-with open('new_dna.txt', 'r', encoding='utf-8') as file:
+with open('../parquet files/new_dna.txt', 'r', encoding='utf-8') as file:
   captions = file.read()
 
 print(f"{(len(captions)/1e6):.2f} million letters")
 
-chars = sorted(list(set(captions)))
-print(chars)
-vocab_size = len(chars)
+from tokenizer import PerCharTokenizer
 
-# map of characters to integers
-stoi = { ch:i for i,ch in enumerate(chars) }
-itos = { i:ch for i,ch in enumerate(chars) }
-encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+tokenizer = PerCharTokenizer()
+vocab_size = tokenizer.vocab_size
 
 import torch
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -29,6 +24,6 @@ model.load_state_dict(checkpoint)
 m = model.to(device)
 
 target_text = "AGTTCTGCGAT"
-context = torch.tensor([encode(target_text)], dtype=torch.long, device=device)
-generated_output = decode(m.generate(context, max_new_tokens=10, temperature=0.5, top_k=5))
+context = torch.tensor([tokenizer.encode(target_text)], dtype=torch.long, device=device)
+generated_output = tokenizer.decode(m.generate(context, max_new_tokens=10, temperature=0.5, top_k=5))
 print(f"{target_text}{generated_output}")
--- enigma/run.py
+++ enigma/run.py
@@ -18,23 +18,17 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 os.chdir(current_directory)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-with open('paraquet files/train1.txt', 'r', encoding='utf-8') as file:
+with open('../parquet files/new_dna.txt', 'r', encoding='utf-8') as file:
   captions = file.read()
 
 print(f"{(len(captions)/1e6):.2f} million letters")
 
-chars = sorted(list(set(captions)))
-print(chars)
-vocab_size = len(chars)
+from ..tokenizer import PerCharTokenizer
 
-# map of characters to integers
-stoi = { ch:i for i,ch in enumerate(chars) }
-itos = { i:ch for i,ch in enumerate(chars) }
-encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
-
+tokenizer = PerCharTokenizer()
+vocab_size = tokenizer.vocab_size
 # Train and test splits
-data = torch.tensor(encode(captions), dtype=torch.long)
+data = torch.tensor(tokenizer.encode(captions), dtype=torch.long)
 n = int(0.9*len(data)) # first 90% will be train, rest val
 train_data = data[:n]
 val_data = data[n:]
@@ -76,7 +70,7 @@ def estimate_loss():
     return out
 
 from model import Transformer
-model = Transformer()
+model = Transformer(vocab_size=vocab_size)
 m = model.to(device)
 
 # no of parameters
@@ -104,8 +98,3 @@ for iter in range(max_iters):
   optimizer.step()
 
 torch.save(model.state_dict(), f'enigma_{n_param:.0f}m.pth')
-
-target_text = "AGTTCTGCGAT"
-context = torch.tensor([encode(target_text)], dtype=torch.long, device=device)
-generated_output = decode(m.generate(context, max_new_tokens=10, temperature=0.5, top_k=5))
-print(f"{target_text}{generated_output}")
