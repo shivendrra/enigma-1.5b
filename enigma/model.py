209bfa709b43f90af3bf31cadfcee17a86d6ee85
Shivendra <shivharsh44@gmail.com>
2024-03-20 23:21:37 +0530

    change in model structure and configurations

--- enigma/model.py
+++ enigma/model.py
@@ -1,6 +1,6 @@
 """
-  simple BERT architecture model, paired with one more layer of 
-  masked self-attention, to predict next token
+  a transformer model with one masked and two un-masked attention layers
+  with the current configuration, it can reach 150million parameters
 """
 
 import torch
@@ -19,26 +19,32 @@ batch_size = params['batch_size']
 block_size = params['block_size']
 n_head = params['n_head']
 d_model = params['d_model']
-n_layer = params['n_layer']
+n_layers = params['n_layer']
 dropout = params['dropout']
 norm_eps = params['norm_eps']
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-class UnMaskedHead(nn.Module):
+class AttentionHead(nn.Module):
   """ single head of self attention """
-  def __init__(self, d_model, head_size, dropout):
+
+  def __init__(self, d_model, head_size, dropout, block_size):
     super().__init__()
     self.key = nn.Linear(d_model, head_size, bias=True)
-    self.query = nn.Linear(d_model, head_size, bias=True) 
-    self.value = nn.Linear(d_model, head_size, bias=True)
+    self.query = nn.Linear(d_model, head_size, bias=True)
+    self.value = nn.Linear(d_model, head_size, bias=False)
     self.dropout = nn.Dropout(dropout)
-  
-  def forward(self, x):
+    self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+  def forward(self, x, mask=False):
     B, T, C = x.shape
     key = self.key(x)
     query = self.query(x)
 
-    weights = query @ key.transpose(-2, -1) * key.shape[-1]**-0.5
+    weights = query @ key.transpose(-2, -1) / (key.shape[-1]**-0.5)
+
+    if mask is True:
+      weights = weights.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+
     weights = F.softmax(weights, dim=-1)
     weights = self.dropout(weights)
 
@@ -46,103 +52,89 @@ class UnMaskedHead(nn.Module):
     out = weights @ value
     return out
 
-class MaskedHead(nn.Module):
-  """ one head of self-attention """
-  def __init__(self, head_size, dropout, d_model):
-    super().__init__()
-    self.key = nn.Linear(d_model, head_size, bias=True)
-    self.query = nn.Linear(d_model, head_size, bias=True)
-    self.value = nn.Linear(d_model, head_size, bias=True)
-    self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
-
-    self.dropout = nn.Dropout(dropout)
-
-  def forward(self, x):
-    B,T,C = x.shape
-    k = self.key(x)
-    q = self.query(x)
-    
-    wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
-    wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
-    wei = F.softmax(wei, dim=-1) # (B, T, T)
-    wei = self.dropout(wei)
-    
-    v = self.value(x)
-    out = wei @ v
-    return out
-  
-class MultiUnMasked(nn.Module):
-  def __init__(self, d_model, n_head, dropout):
+class MultiHeadAttention(nn.Module):
+  def __init__(self, d_model, n_head, dropout, block_size):
     head_size = d_model // n_head
     super().__init__()
-    self.heads = nn.ModuleList([UnMaskedHead(d_model=d_model, dropout=dropout, head_size=head_size) for _ in range(n_head)])
+    self.heads = nn.ModuleList([AttentionHead(d_model=d_model, dropout=dropout, head_size=head_size, block_size=block_size) for _ in range(n_head)])
     self.proj = nn.Linear(n_head * head_size, d_model)
     self.dropout = nn.Dropout(dropout)
-  
-  def forward(self, x):
-    out = torch.cat([h(x) for h in self.heads], dim=-1)
-    out = self.dropout(self.proj(out))
-    return out
 
-class MultiMasked(nn.Module):
-  def __init__(self, d_model, n_head, dropout):
-    head_size = d_model // n_head
-    super().__init__()
-    self.heads = nn.ModuleList([MaskedHead(d_model=d_model, dropout=dropout, head_size=head_size) for _ in range(n_head)])
-    self.proj = nn.Linear(n_head * head_size, d_model)
-    self.dropout = nn.Dropout(dropout)
-  
-  def forward(self, x):
-    out = torch.cat([h(x) for h in self.heads], dim=-1)
+  def forward(self, x, mask):
+    out = torch.cat([h(x, mask=mask) for h in self.heads], dim=-1)
     out = self.dropout(self.proj(out))
     return out
 
 class FeedForward(nn.Module):
-  """ feed forward network, with 2 linear layers, LeakyRelu for activation function
-    and final layer normalization
-    - expansion factor is 5  
-    - n_feedForward = d_model*5
-  """
   def __init__(self, d_model, dropout):
     super().__init__()
     self.net = nn.Sequential(
-      nn.Linear(d_model, 5*d_model),
-      nn.LeakyReLU(),
-      nn.Linear(5*d_model, d_model),
-      nn.Dropout(dropout),
-      nn.LayerNorm(d_model, eps=1e-5)
+      nn.Linear(d_model, 4*d_model),
+      nn.GELU(),
+      nn.Linear(4*d_model, d_model),
+      nn.Dropout(dropout)
     )
-   
+
   def forward(self, x):
     return self.net(x)
 
-class Block(nn.Module):
-  def __init__(self, d_model, n_head, norm_eps, dropout):
+class EncoderNetwork(nn.Module):
+  def __init__(self, d_model, n_head, norm_eps, dropout, block_size):
     super().__init__()
-    self.sa_masked = MultiMasked(n_head=n_head, d_model=d_model, dropout=dropout)
-    self.sa_unmasked = MultiUnMasked(n_head=n_head, d_model=d_model, dropout=dropout)
-    self.ffwd = FeedForward(d_model, dropout=dropout)
+    self.s_att = MultiHeadAttention(n_head=n_head, d_model=d_model, dropout=dropout, block_size=block_size)
+    self.ffwd = FeedForward(d_model, dropout)
+    self.dropout = nn.Dropout(dropout)
     self.norm1 = nn.LayerNorm(d_model, eps=norm_eps)
     self.norm2 = nn.LayerNorm(d_model, eps=norm_eps)
-  
-  def forward(self, x):
-    x2 = x + self.sa_unmasked(self.norm1(x))
-    x = x2 + self.norm1(self.ffwd(self.norm2(x2)))
 
-    x2 = x + self.sa_masked(self.norm1(x))
-    x = x2 + self.norm2(self.ffwd(self.norm2(x2)))
-    return x
+  def forward(self, src):
+    src2 = self.s_att(src, mask=False)
+    src = src + self.dropout(src2)
+    src = self.norm1(src)
+
+    src2 = self.ffwd(src)
+    src = src + self.dropout(src2)
+    src = self.norm2(src)
+
+    return src
+
+class DecoderNetwork(nn.Module):
+  def __init__(self, d_model, n_head, norm_eps, dropout, block_size):
+    super().__init__()
+    self.s_att = MultiHeadAttention(n_head=n_head, d_model=d_model, dropout=dropout, block_size=block_size)
+    self.ffwd = FeedForward(d_model, dropout)
+    self.dropout = nn.Dropout(dropout)
+    self.norm1 = nn.LayerNorm(d_model, eps=norm_eps)
+    self.norm2 = nn.LayerNorm(d_model, eps=norm_eps)
+
+  def forward(self, src, trg):
+    src2 = self.s_att(src, mask=True)
+    src = src + self.dropout(src2)
+    src = src + self.norm1(src)
+
+    trg2 = self.s_att(trg, mask=False)
+    trg = trg + self.dropout(trg2)
+    trg = trg + self.norm1(trg)
+
+    src_f = src + trg
+    src_f2 = self.ffwd(self.norm2(src_f))
+    src_f = src_f + self.dropout(src_f2)
+    src_f = self.norm2(src_f)
+
+    return src_f
 
 class Transformer(nn.Module):
   def __init__(self, vocab_size):
     super().__init__()
     self.toked_model = nn.Embedding(vocab_size, d_model)
     self.pos_encod = nn.Embedding(block_size, d_model)
-    self.block = nn.Sequential(*[Block(d_model=d_model, dropout=dropout, norm_eps=norm_eps, n_head=n_head) for _ in range(n_layer)])
-    self.norm_final = nn.LayerNorm(d_model, eps=norm_eps)
+    self.enc_layer = nn.ModuleList([EncoderNetwork(n_head=n_head, norm_eps=norm_eps, block_size=block_size, dropout=dropout, d_model=d_model) for _ in range(n_layers)])
+    self.dec_layer = nn.ModuleList([DecoderNetwork(n_head=n_head, norm_eps=norm_eps, block_size=block_size, dropout=dropout, d_model=d_model) for _ in range(n_layers)])
+
+    self.norm_final = nn.LayerNorm(d_model)
     self.linear_final = nn.Linear(d_model, vocab_size)
+    self.dropout = nn.Dropout(dropout)
     self.apply(self._init_weights)
-  
 
   def _init_weights(self, module):
     if isinstance(module, nn.Linear):
@@ -151,28 +143,34 @@ class Transformer(nn.Module):
         torch.nn.init.zeros_(module.bias.data)
     elif isinstance(module, nn.Embedding):
       torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
-    
+
   def forward(self, idx, targets=None):
     B, T = idx.shape
 
     toked_model = self.toked_model(idx)
     pos_encod = self.pos_encod(torch.arange(T, device=device))
     x = toked_model + pos_encod
-    x = self.block(x)
+
+    for layer in self.enc_layer:
+      x = layer(x)
+
+    for layer in self.dec_layer:
+      x = layer(x, x)
+
     x = self.norm_final(x)
     logits = self.linear_final(x)
 
     if targets is None:
       loss = None
-    
+
     else:
       B, T, C = logits.shape
       logits = logits.view(B*T, C)
       targets = targets.view(B*T)
       loss = F.cross_entropy(logits, targets)
-    
+
     return logits, loss
-  
+
   def generate(self, idx, max_new_tokens, temperature=1.0, top_k=0):
     generated_tokens = []
 
@@ -196,4 +194,5 @@ class Transformer(nn.Module):
     values, indices = torch.topk(logits, top_k, dim=-1)
     min_value = values[:, -1].unsqueeze(-1).expand_as(logits)
     filtered_logits = torch.where(logits < min_value, torch.ones_like(logits) * -float('inf'), logits)
+
     return filtered_logits
