9733bb5965a423c27e9a7ef4316105e43e10b247
Shivendra <shivharsh44@gmail.com>
2024-04-10 23:57:34 +0530

    fixed issues with decoder

--- dna_test.py
+++ dna_test.py
@@ -4,26 +4,25 @@ os.chdir(current_dir)
 
 from tokenizer import KMerTokenizer
 
-with open('../parquet files/extra.txt', 'r', encoding='utf-8') as f:
+with open('../parquet files/train.txt', 'r', encoding='utf-8') as f:
   train_data = f.read()
   print("file opened!")
 
-tokenizer = KMerTokenizer(k_mers=5)
+tokenizer = KMerTokenizer(k_mers=3)
 tokenizer.build_vocab([train_data])
 tokenizer.save_model('../tokenizer/trained models')
 
-with open('../parquet files/train.txt', 'r', encoding='utf-8') as f:
-  test_data = f.read()
-  print("file opened!")
+# with open('../parquet files/train1.txt', 'r', encoding='utf-8') as f:
+#   test_data = f.read()
+#   print("file opened!")
 
-tokenizer.load_model('../tokenizer/trained models/base_5k.json')
+# tokenizer.load_model('../tokenizer/trained models/base_5k.json')
 
-encoded_tokens = tokenizer.encode(test_data)
-decoded_tokens = tokenizer.decode(encoded_tokens)
-print(encoded_tokens)
-print(decoded_tokens)
+# encoded_tokens = tokenizer.encode(test_data)
+# decoded_tokens = tokenizer.decode(encoded_tokens)
+# print(decoded_tokens)
 
-print(f"seq length: {len(test_data)} \ntokens length: {len(decoded_tokens)}")
-print(test_data == decoded_tokens)
-print(f"file length: {len(test_data)} \ntokens: {len(encoded_tokens)}")
-print(f"compression ration: {(len(test_data) / len(encoded_tokens)):.2f}x")
+# print(f"seq length: {len(test_data)} \ntokens length: {len(decoded_tokens)}")
+# print(test_data == decoded_tokens)
+# print(f"file length: {len(test_data)} \ntokens: {len(encoded_tokens)}")
+# print(f"compression ration: {(len(test_data) / len(encoded_tokens)):.2f}x")
--- tokenizer/kmer.py
+++ tokenizer/kmer.py
@@ -36,23 +36,34 @@ class KMerTokenizer:
       if kmer in self.token_to_id:
         encoded_sequence.append(self.token_to_id[kmer])
       else:
-        encoded_sequence.append(len(self.vocab))
+        break
     return encoded_sequence
 
   def decode(self, encoded_sequence):
-    decoded_sequence = [self.id_to_token[token_id] for token_id in encoded_sequence]
-    return decoded_sequence
+    decoded_tokens = []
+    for token_id in encoded_sequence:
+      if token_id < len(self.id_to_token):
+        decoded_tokens.append(self.id_to_token[token_id])
+      else:
+        break
+    return ''.join(decoded_tokens)
   
   def save_model(self, model_path):
     vocab_file = f"{model_path}/base_{self.k_mers}k.json"
     with open(vocab_file, 'w') as f:
       json.dump(self.vocab, f)
+    print("saved the vocab!")
 
   def load_model(self, path):
     assert path.endswith('.json')
     with open(path, 'r') as f:
       vocab = json.load(f)
+    print("loaded the vocab!")
     
     self.vocab = vocab
     self.token_to_id = self.vocab
     self.vocab_size = len(vocab)
+
+    self.id_to_token = [None] * self.vocab_size
+    for token, idx in self.vocab.items():
+        self.id_to_token[idx] = token
